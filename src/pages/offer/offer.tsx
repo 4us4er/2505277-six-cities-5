017b921b7cfaa2137d58427d4bf7bae41acaddcc
4us4e <145545210+4us4er@users.noreply.github.com>
2025-04-23 02:12:33 +0700

    feat: add component for Header

--- src/pages/offer/offer.tsx
+++ src/pages/offer/offer.tsx
@@ -17,6 +17,7 @@ import {
 } from '../../store/api-actions';
 import { store } from '../../store/store';
 import { getOffers } from '../../store/offers-data/selectors';
+import { Header } from '../../components/header/header';
 
 function Offer(): JSX.Element {
   const [reviews, setReviews] = useState<Comments[]>([]);
@@ -82,45 +83,7 @@ function Offer(): JSX.Element {
 
   return (
     <div className="page">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link" href="#">
-                <img
-                  className="header__logo"
-                  src="img/logo.svg"
-                  alt="6 cities logo"
-                  width="81"
-                  height="41"
-                />
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a
-                    className="header__nav-link header__nav-link--profile"
-                    href="#"
-                  >
-                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                    <span className="header__user-name user__name">
-                      Oliver.conner@gmail.com
-                    </span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
-
+      <Header />
       <main className="page__main page__main--offer">
         <section className="offer">
           <div className="offer__gallery-container container">
@@ -158,8 +121,7 @@ function Offer(): JSX.Element {
                         selectOffer?.rating ? selectOffer?.rating * 20 : 20 * 3
                       }%`,
                     }}
-                  >
-                  </span>
+                  ></span>
                   <span className="visually-hidden">Rating</span>
                 </div>
                 <span className="offer__rating-value rating__value">
